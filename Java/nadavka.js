b17ab629cf32422ddf41bc2afd28609f68d9f2dc
KyuuHere <s3czsk@seznam.cz>
2026-01-11 20:52:54 +0100

    qw

--- Java/nadavka.js
+++ Java/nadavka.js
@@ -1,9 +1,9 @@
 const drivers2025 = [
-    { name: "Lando Norris", team: "McLaren", points: 423 }
+    { name: " Lando Norris ", team: " McLaren", points: 423 }
 ];
 
 const teams2025 = [
-    { team: "McLaren", points: 800 }
+    { team: " McLaren", points: 800 }
 ];
 
 // mělo by to doplnit tabulku drivers(snad)
@@ -14,9 +14,9 @@ drivers2025.forEach((driver, index) => {
     driversT.innerHTML += `
         <tr>
             <td>${index + 1}</td>
-            <td>${driver.name}</td>
-            <td>${driver.team}</td>
-            <td>${driver.points}</td>
+            <td>${driver.name }</td>
+            <td>${driver.team }</td>
+            <td>${driver.points }</td>
         </tr>
     `;
 });
@@ -28,15 +28,15 @@ teams2025.sort((a, b) => b.points - a.points);
 teams2025.forEach((team, index) => {
     teamsT.innerHTML += `
         <tr>
-            <td>${index + 1}</td>
-            <td>${team.team}</td>
-            <td>${team.points}</td>
+            <td>${index + 1 }</td>
+            <td>${team.team }</td>
+            <td>${team.points }</td>
         </tr>
     `;
 });
 
 const fastest2025=[
-       { race: "Bahrain", driver: "Max Verstappen", time:"1:32.456" }
+       { race: "Bahrain", driver: "Max Verstappen", time:" 1:32.456" }
 ];
 
 // mělo by to doplnit tabulku kol (snad)
@@ -44,9 +44,9 @@ const fastest=document.getElementById("fastest");
 fastest2025.forEach(lap=>{
    fastest.innerHTML +=`
         <tr>
-            <td>${lap.race}</td>
-            <td>${lap.driver}</td>
-            <td>${lap.time}</td>
+            <td>${lap.race }</td>
+            <td>${lap.driver }</td>
+            <td>${lap.time }</td>
         </tr>
     `;
 })
